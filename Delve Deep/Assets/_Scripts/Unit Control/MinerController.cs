b652f9b1a72b4984bbba6a669dc7b468276234f5
bwbelcher12 <bwbelcher12@gmail.com>
2022-12-10 15:51:49 -0500

    Improved pathfinding some more

--- Delve Deep/Assets/_Scripts/Unit Control/MinerController.cs
+++ Delve Deep/Assets/_Scripts/Unit Control/MinerController.cs
@@ -9,6 +9,7 @@ public class MinerController : RTSUnitControllerScript
 {
     [SerializeField] List<GameObject> _targetNodes;
     private bool mining;
+    private bool destinationIsMiningNode;
 
     public void AddTarget(GameObject node)
     {
@@ -93,6 +94,11 @@ public class MinerController : RTSUnitControllerScript
     public override void GoToTarget(Vector3 target)
     {
         _agent.destination = target;
+        if (destinationIsMiningNode.Equals(false))
+        {
+            _targetNodes.Clear();
+        }
+
     }
 
     public override Vector3 CheckHit(RaycastHit hit)
@@ -102,13 +108,15 @@ public class MinerController : RTSUnitControllerScript
 
         if (hit.collider.transform.CompareTag("MiningNode").Equals(true))
         {
+            destinationIsMiningNode = true;
+
             AddTarget(hit.collider.transform.gameObject);
 
             float distance = Mathf.Infinity;
 
             Vector3 returnSpot = hit.collider.transform.position;
 
-            foreach(Transform miningSpot in hit.collider.transform)
+            foreach(Transform miningSpot in hit.collider.transform.GetComponent<MiningNode>().minerPositions.transform)
             {
                 float tempDist = Vector3.Distance(miningSpot.position, transform.position);
                 if(tempDist < distance)
